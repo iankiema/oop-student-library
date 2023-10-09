6b6b61234293347d731b0441054b6ff2250b6247
iankiema <kiemaian.ik@gmail.com>
2023-10-09 16:36:48 +0300

    Updated the teacher.rb file

--- teacher.rb
+++ teacher.rb
@@ -2,7 +2,7 @@ require_relative 'person'
 
 class Teacher < Person
   def initialize(specialization:, name: 'Unknown', age: 0)
-    super(name: name, age: age)
+    super(name: name, age: age, parent_permission: parent_permission)
     @specialization = specialization
   end
 
