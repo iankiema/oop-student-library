f4a530e84c6beab764fbc2332d5dbe12220371bb
iankiema <kiemaian.ik@gmail.com>
2023-10-09 19:54:55 +0300

    Updated the person.rb file and added tests.rb

--- person.rb
+++ person.rb
@@ -1,6 +1,4 @@
 require_relative 'nameable'
-require_relative 'capitalize_decorator'
-require_relative 'trimmer_decorator'
 
 class Person < Nameable
   attr_accessor :name, :age
@@ -32,11 +30,3 @@ class Person < Nameable
     rand(1..1000)
   end
 end
-
-person = Person.new(name: 'maximilianus', age: 25)
-capitalized_person = CapitalizeDecorator.new(person)
-trimmed_person = TrimmerDecorator.new(capitalized_person)
-
-puts person.correct_name
-puts capitalized_person.correct_name
-puts trimmed_person.correct_name
