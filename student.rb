5c523be10f23f6d3f70f8e2f680cb27db833dce8
iankiema <kiemaian.ik@gmail.com>
2023-10-10 12:53:43 +0300

    Added book.rb,classroom.rb,rental.rb and updated person.rb,student.rb and tests.rb file

--- student.rb
+++ student.rb
@@ -1,6 +1,8 @@
 require_relative 'person'
 
 class Student < Person
+  attr_accessor :name, :classroom
+
   def initialize(classroom:, name: 'Unknown', age: 0, parent_permission: true)
     super(name: name, age: age, parent_permission: parent_permission)
     @classroom = classroom
